8e66de100cd6cd8394d2d15ff914816bf46c0b9b
hachemchrayti <hachemfst@gmail.Com>
2021-03-23 12:03:56 +0100

    Registr new User form(POST) & modify default aspnet core password complexity

--- EmployeeManagementProject/Startup.cs
+++ EmployeeManagementProject/Startup.cs
@@ -29,7 +29,14 @@ namespace EmployeeManagementProject
 
             services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 10;
+                options.Password.RequiredUniqueChars = 3;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>();
+
             services.AddMvc(options => options.EnableEndpointRouting = false);
             //services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
