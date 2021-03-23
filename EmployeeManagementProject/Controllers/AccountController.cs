80c622e184ad4b858c107a168fac2ce084b4230d
hachemchrayti <hachemfst@gmail.Com>
2021-03-23 11:51:55 +0100

    Registr new User form(GET)

---
+++ EmployeeManagementProject/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
