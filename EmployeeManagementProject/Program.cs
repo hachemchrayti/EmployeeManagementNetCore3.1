9b8ac166200295bedac7107eb544cb9b648ea8be
hachemchrayti <hachemfst@gmail.Com>
2021-03-23 10:49:14 +0100

    Add logging with NLog to a custom file log

--- EmployeeManagementProject/Program.cs
+++ EmployeeManagementProject/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using NLog.Extensions.Logging;
 namespace EmployeeManagementProject
 {
     public class Program
@@ -17,7 +17,16 @@ namespace EmployeeManagementProject
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).
+            ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                //logging.AddConsole();
+                //logging.AddDebug();
+                //logging.AddEventSourceLogger();
+                // Enable NLog as one of the Logging Provider
+                logging.AddNLog();
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
