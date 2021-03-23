5d53b0370f98c87438ab3af0548f3b84d0b745d7
hachemchrayti <hachemfst@gmail.Com>
2021-03-23 16:21:23 +0100

    Add Custo validation attribute ' email domain must be gmail.com' in the register form

--- EmployeeManagementProject/ViewModels/RegisterViewModel.cs
+++ EmployeeManagementProject/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using EmployeeManagementProject.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagementProject.ViewModels
@@ -9,6 +10,8 @@ namespace EmployeeManagementProject.ViewModels
         [Required]
         [EmailAddress]
         [Remote(action: "IsEmailInUse", controller: "Account")]
+        [ValidEmailDomain(allowedDomain: "gmail.com",
+        ErrorMessage = "Email domain must be gmail.com")]
         public string Email { get; set; }
 
         [Required]
